69ca216230f547fc5e25950fc64b1d36bbfa0f81
shashank <shashank.khochikar@gmail.com>
2019-04-26 15:56:16 +0530

    Initial commit

---
+++ app/src/main/java/com/brahamaputra/mahindra/brahamaputravendor/Activities/ItemListActivity.java
@@ -0,0 +1,15 @@
+package com.brahamaputra.mahindra.brahamaputravendor.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.brahamaputra.mahindra.brahamaputravendor.R;
+
+public class ItemListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+    }
+}
